aee998bcf9569a9085ee0d99166939addfba31c2
Ircama <Ircama>
2025-10-18 01:45:46 +0200

    SQLite: Add version selection and fix LIB_VERSION
    - Add support for multiple SQLite versions (3.40.1 ABANDON vs 3.50.4 CURRENT)
    - Fix LIB_VERSION to use correct versions (0.8.6 for ABANDON, 3.50.4 for CURRENT)
    - Add Config.in.libs for library version selection
    - Update external.files and external.in for proper externalization
    - Update patches for current version

--- docs/make/sqlite.md
+++ docs/make/sqlite.md
@@ -1,4 +1,4 @@
-# SQLite 3.40.1/3.47.1 (binary only) - DEPRECATED
+# SQLite 3.50.4 (binary and library)
   - Homepage: [https://www.sqlite.org](https://www.sqlite.org)
   - Manpage: [https://www.sqlite.org/docs.html](https://www.sqlite.org/docs.html)
   - Changelog: [https://www.sqlite.org/changes.html](https://www.sqlite.org/changes.html)
@@ -6,3 +6,18 @@
   - Package: [master/make/pkgs/sqlite/](https://github.com/Freetz-NG/freetz-ng/tree/master/make/pkgs/sqlite/)
   - Maintainer: -
 
+## Source
+- Version: 3.50.4
+- Source URL: https://www.sqlite.org/2025/sqlite-autoconf-3500400.tar.gz
+- Hash: a3db587a1b92ee5ddac2f66b3edb41b26f9c867275782d46c3a088977d6a5b18
+
+## Build Notes
+SQLite 3.50.4 uses autosetup (Tcl-based) instead of GNU autotools for configuration. This affects the available configure options:
+- Unsupported options like `--cache-file`, `--target`, `--disable-nls` are ignored or cause errors.
+- The build system creates files directly in the source directory (e.g., `sqlite3`, `libsqlite3.so`) rather than in `.libs/`. The `make install` then copies them to the staging directory, from where Freetz installs them to the target root filesystem.
+- Installation uses `/usr/local` prefix by default, requiring adjustments in `sqlite.mk` for staging paths.
+
+## Externalization
+Both the SQLite CLI (`sqlite3`) and the library (`libsqlite3.so`) can be externalized:
+- Binary: `/usr/bin/sqlite3` (~328 KB)
+- Library: `/usr/lib/freetz/libsqlite3.so.0` (symlink to `libsqlite3.so.3.50.4`, ~1.3 MB)
--- make/pkgs/sqlite/sqlite.mk
+++ make/pkgs/sqlite/sqlite.mk
@@ -1,13 +1,15 @@
-$(call PKG_INIT_BIN, $(if $(FREETZ_LIB_libsqlite3_WITH_VERSION_ABANDON),3400100,3470100))
-$(PKG)_LIB_VERSION:=0.8.6
+$(call PKG_INIT_BIN, $(if $(FREETZ_LIB_libsqlite3_WITH_VERSION_ABANDON),3400100,3500400))
+$(PKG)_LIB_VERSION_ABANDON:=0.8.6
+$(PKG)_LIB_VERSION_CURRENT:=3.50.4
+$(PKG)_LIB_VERSION:=$($(PKG)_LIB_VERSION_$(if $(FREETZ_LIB_libsqlite3_WITH_VERSION_ABANDON),ABANDON,CURRENT))
 $(PKG)_SOURCE:=$(pkg)-autoconf-$($(PKG)_VERSION).tar.gz
 $(PKG)_HASH_ABANDON:=2c5dea207fa508d765af1ef620b637dcb06572afa6f01f0815bd5bbf864b33d9
-$(PKG)_HASH_CURRENT:=416a6f45bf2cacd494b208fdee1beda509abda951d5f47bc4f2792126f01b452
+$(PKG)_HASH_CURRENT:=a3db587a1b92ee5ddac2f66b3edb41b26f9c867275782d46c3a088977d6a5b18
 $(PKG)_HASH:=$($(PKG)_HASH_$(if $(FREETZ_LIB_libsqlite3_WITH_VERSION_ABANDON),ABANDON,CURRENT))
 $(PKG)_SITE_ABANDON:=https://www.sqlite.org/2022
-$(PKG)_SITE_CURRENT:=https://www.sqlite.org/2024
+$(PKG)_SITE_CURRENT:=https://www.sqlite.org/2025
 $(PKG)_SITE:=$($(PKG)_SITE_$(if $(FREETZ_LIB_libsqlite3_WITH_VERSION_ABANDON),ABANDON,CURRENT))
-### VERSION:=3.40.1/3.47.1
+### VERSION:=3.40.1/3.50.4
 ### WEBSITE:=https://www.sqlite.org
 ### MANPAGE:=https://www.sqlite.org/docs.html
 ### CHANGES:=https://www.sqlite.org/changes.html
@@ -19,7 +21,7 @@ endif
 
 $(PKG)_CONDITIONAL_PATCHES+=$(if $(FREETZ_LIB_libsqlite3_WITH_VERSION_ABANDON),abandon,current)
 
-$(PKG)_BINARY:=$($(PKG)_DIR)/.libs/sqlite3
+$(PKG)_BINARY:=$($(PKG)_DIR)/sqlite3
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/sqlite3
 
 $(PKG)_LIB_BINARY:=$($(PKG)_DIR)/.libs/libsqlite3.so.$($(PKG)_LIB_VERSION)
@@ -28,18 +30,25 @@ $(PKG)_LIB_TARGET_BINARY:=$($(PKG)_TARGET_LIBDIR)/libsqlite3.so.$($(PKG)_LIB_VER
 
 $(PKG)_REBUILD_SUBOPTS += FREETZ_LIB_libsqlite3_WITH_VERSION_ABANDON
 
-$(PKG)_CONFIGURE_OPTIONS += --enable-shared
-$(PKG)_CONFIGURE_OPTIONS += --enable-static
-$(PKG)_CONFIGURE_OPTIONS += --disable-editline
-$(PKG)_CONFIGURE_OPTIONS += --disable-static-shell
-$(PKG)_CONFIGURE_OPTIONS += $(if $(FREETZ_PACKAGE_SQLITE_WITH_READLINE),--enable-readline,--disable-readline)
-
-$(PKG)_CONFIGURE_ENV += ac_cv_header_zlib_h=no
-
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
-$(PKG_CONFIGURED_CONFIGURE)
+
+# SQLite uses a non-standard configure script (jimtcl-based)
+# It doesn't support standard autoconf options like --target, --cache-file etc
+$($(PKG)_DIR)/.configured: $($(PKG)_DIR)/.unpacked
+	( cd $(SQLITE_DIR); \
+		$(TARGET_CONFIGURE_ENV) \
+		ac_cv_header_zlib_h=no \
+		./configure \
+		--prefix=/usr \
+		--enable-shared \
+		--enable-static \
+		--disable-editline \
+		--disable-static-shell \
+		$(if $(FREETZ_PACKAGE_SQLITE_WITH_READLINE),--enable-readline,--disable-readline) \
+	)
+	touch $@
 
 $($(PKG)_LIB_BINARY): $($(PKG)_DIR)/.configured
 	$(SUBMAKE) -C $(SQLITE_DIR)
@@ -52,8 +61,7 @@ $($(PKG)_LIB_STAGING_BINARY): $($(PKG)_LIB_BINARY)
 		DESTDIR="$(TARGET_TOOLCHAIN_STAGING_DIR)" \
 		all install
 	$(PKG_FIX_LIBTOOL_LA) \
-		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/pkgconfig/sqlite3.pc \
-		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libsqlite3.la
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/pkgconfig/sqlite3.pc
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 	$(INSTALL_BINARY_STRIP)
@@ -69,8 +77,9 @@ $(pkg)-precompiled: $($(PKG)_TARGET_BINARY) $($(PKG)_LIB_TARGET_BINARY)
 $(pkg)-clean:
 	-$(SUBMAKE) -C $(SQLITE_DIR) clean
 	$(RM) -r $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libsqlite3* \
-		$(TARGET_TOOLCHAIN_STAGING_DIR)/lib/pkgconfig/sqlite3.pc \
-		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include/sqlite \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/pkgconfig/sqlite3.pc \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include/sqlite3.h \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include/sqlite3ext.h \
 		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/bin/sqlite3*
 
 $(pkg)-uninstall:
