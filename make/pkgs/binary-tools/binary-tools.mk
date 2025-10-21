dba18cd53598c6eb74f423ed531dae5da253bddf
Ircama <Ircama>
2025-10-21 22:57:52 +0200

    Bug fixing

--- make/pkgs/binary-tools/binary-tools.mk
+++ make/pkgs/binary-tools/binary-tools.mk
@@ -8,44 +8,138 @@ $(PKG)_SITE:=https://ftp.gnu.org/gnu/binutils,https://mirror.dogado.de/gnu/binut
 ### CVSREPO:=https://sourceware.org/git/binutils-gdb.git
 ### SUPPORT:=Ircama
 
-# Patchelf configuration (reuse patchelf-host source)
-# Note: We don't define _PATCHELF_SOURCE/_HASH here to avoid conflicts with patchelf-host
-# Instead, we'll reference PATCHELF_HOST variables directly
-$(PKG)_PATCHELF_DIR:=$($(PKG)_SOURCE_DIR)/patchelf-$(PATCHELF_HOST_VERSION)
+# Patchelf configuration - using version 0.18.0 with dynamic libstdc++ linking
+BINARY_TOOLS_PATCHELF_VERSION:=0.18.0
+BINARY_TOOLS_PATCHELF_SOURCE:=patchelf-$(BINARY_TOOLS_PATCHELF_VERSION).tar.bz2
+BINARY_TOOLS_PATCHELF_HASH:=e9dc4d53c2db7a31fd2c0d0e4b0e6b89d2d87e3fb1ba92b001f8f32432bb3444
+BINARY_TOOLS_PATCHELF_SITE:=https://github.com/NixOS/patchelf/releases/download/$(BINARY_TOOLS_PATCHELF_VERSION)
+BINARY_TOOLS_PATCHELF_DIR:=$(BINARY_TOOLS_SOURCE_DIR)/patchelf-$(BINARY_TOOLS_PATCHELF_VERSION)
 
 # Define build and target locations for each binary following file.mk pattern
-$(PKG)_READELF_BUILD := $($(PKG)_DIR)/binutils/readelf
-$(PKG)_READELF_TARGET := $($(PKG)_DEST_DIR)/usr/bin/readelf
+BINARY_TOOLS_READELF_BUILD := $(BINARY_TOOLS_DIR)/binutils/readelf
+BINARY_TOOLS_READELF_TARGET := $(BINARY_TOOLS_DEST_DIR)/usr/bin/readelf
 
-$(PKG)_OBJDUMP_BUILD := $($(PKG)_DIR)/binutils/objdump
-$(PKG)_OBJDUMP_TARGET := $($(PKG)_DEST_DIR)/usr/bin/objdump
+BINARY_TOOLS_OBJDUMP_BUILD := $(BINARY_TOOLS_DIR)/binutils/objdump
+BINARY_TOOLS_OBJDUMP_TARGET := $(BINARY_TOOLS_DEST_DIR)/usr/bin/objdump
 
-$(PKG)_OBJCOPY_BUILD := $($(PKG)_DIR)/binutils/objcopy
-$(PKG)_OBJCOPY_TARGET := $($(PKG)_DEST_DIR)/usr/bin/objcopy
+BINARY_TOOLS_OBJCOPY_BUILD := $(BINARY_TOOLS_DIR)/binutils/objcopy
+BINARY_TOOLS_OBJCOPY_TARGET := $(BINARY_TOOLS_DEST_DIR)/usr/bin/objcopy
 
-$(PKG)_NM_BUILD := $($(PKG)_DIR)/binutils/nm-new
-$(PKG)_NM_TARGET := $($(PKG)_DEST_DIR)/usr/bin/nm
+BINARY_TOOLS_NM_BUILD := $(BINARY_TOOLS_DIR)/binutils/nm-new
+BINARY_TOOLS_NM_TARGET := $(BINARY_TOOLS_DEST_DIR)/usr/bin/nm
 
-$(PKG)_STRINGS_BUILD := $($(PKG)_DIR)/binutils/strings
-$(PKG)_STRINGS_TARGET := $($(PKG)_DEST_DIR)/usr/bin/strings
+BINARY_TOOLS_STRINGS_BUILD := $(BINARY_TOOLS_DIR)/binutils/strings
+BINARY_TOOLS_STRINGS_TARGET := $(BINARY_TOOLS_DEST_DIR)/usr/bin/strings
 
-$(PKG)_AR_BUILD := $($(PKG)_DIR)/binutils/ar
-$(PKG)_AR_TARGET := $($(PKG)_DEST_DIR)/usr/bin/ar
+BINARY_TOOLS_AR_BUILD := $(BINARY_TOOLS_DIR)/binutils/ar
+BINARY_TOOLS_AR_TARGET := $(BINARY_TOOLS_DEST_DIR)/usr/bin/ar
 
-$(PKG)_RANLIB_BUILD := $($(PKG)_DIR)/binutils/ranlib
-$(PKG)_RANLIB_TARGET := $($(PKG)_DEST_DIR)/usr/bin/ranlib
+BINARY_TOOLS_RANLIB_BUILD := $(BINARY_TOOLS_DIR)/binutils/ranlib
+BINARY_TOOLS_RANLIB_TARGET := $(BINARY_TOOLS_DEST_DIR)/usr/bin/ranlib
 
-$(PKG)_STRIP_BUILD := $($(PKG)_DIR)/binutils/strip-new
-$(PKG)_STRIP_TARGET := $($(PKG)_DEST_DIR)/usr/bin/strip
+BINARY_TOOLS_STRIP_BUILD := $(BINARY_TOOLS_DIR)/binutils/strip-new
+BINARY_TOOLS_STRIP_TARGET := $(BINARY_TOOLS_DEST_DIR)/usr/bin/strip
 
-$(PKG)_ADDR2LINE_BUILD := $($(PKG)_DIR)/binutils/addr2line
-$(PKG)_ADDR2LINE_TARGET := $($(PKG)_DEST_DIR)/usr/bin/addr2line
+BINARY_TOOLS_ADDR2LINE_BUILD := $(BINARY_TOOLS_DIR)/binutils/addr2line
+BINARY_TOOLS_ADDR2LINE_TARGET := $(BINARY_TOOLS_DEST_DIR)/usr/bin/addr2line
 
-$(PKG)_SIZE_BUILD := $($(PKG)_DIR)/binutils/size
-$(PKG)_SIZE_TARGET := $($(PKG)_DEST_DIR)/usr/bin/size
+BINARY_TOOLS_SIZE_BUILD := $(BINARY_TOOLS_DIR)/binutils/size
+BINARY_TOOLS_SIZE_TARGET := $(BINARY_TOOLS_DEST_DIR)/usr/bin/size
 
-$(PKG)_PATCHELF_BUILD := $($(PKG)_PATCHELF_DIR)/src/patchelf
-$(PKG)_PATCHELF_TARGET := $($(PKG)_DEST_DIR)/usr/bin/patchelf
+BINARY_TOOLS_PATCHELF_BUILD := $(BINARY_TOOLS_PATCHELF_DIR)/src/patchelf
+BINARY_TOOLS_PATCHELF_TARGET := $(BINARY_TOOLS_DEST_DIR)/usr/bin/patchelf
+
+# Conditional tool inclusion based on user selection
+BINARY_TOOLS_SELECTED :=
+BINARY_TOOLS_TARGETS_PRECOMPILED :=
+
+ifeq ($(strip $(FREETZ_PACKAGE_BINARY_TOOLS_READELF)),y)
+BINARY_TOOLS_SELECTED += readelf
+BINARY_TOOLS_TARGETS_PRECOMPILED += $(BINARY_TOOLS_READELF_TARGET)
+$(PKG)_EXCLUDED := $(filter-out usr/bin/readelf,$($(PKG)_EXCLUDED))
+else
+$(PKG)_EXCLUDED += usr/bin/readelf
+endif
+
+ifeq ($(strip $(FREETZ_PACKAGE_BINARY_TOOLS_OBJDUMP)),y)
+BINARY_TOOLS_SELECTED += objdump
+BINARY_TOOLS_TARGETS_PRECOMPILED += $(BINARY_TOOLS_OBJDUMP_TARGET)
+$(PKG)_EXCLUDED := $(filter-out usr/bin/objdump,$($(PKG)_EXCLUDED))
+else
+$(PKG)_EXCLUDED += usr/bin/objdump
+endif
+
+ifeq ($(strip $(FREETZ_PACKAGE_BINARY_TOOLS_OBJCOPY)),y)
+BINARY_TOOLS_SELECTED += objcopy
+BINARY_TOOLS_TARGETS_PRECOMPILED += $(BINARY_TOOLS_OBJCOPY_TARGET)
+$(PKG)_EXCLUDED := $(filter-out usr/bin/objcopy,$($(PKG)_EXCLUDED))
+else
+$(PKG)_EXCLUDED += usr/bin/objcopy
+endif
+
+ifeq ($(strip $(FREETZ_PACKAGE_BINARY_TOOLS_NM)),y)
+BINARY_TOOLS_SELECTED += nm
+BINARY_TOOLS_TARGETS_PRECOMPILED += $(BINARY_TOOLS_NM_TARGET)
+$(PKG)_EXCLUDED := $(filter-out usr/bin/nm,$($(PKG)_EXCLUDED))
+else
+$(PKG)_EXCLUDED += usr/bin/nm
+endif
+
+ifeq ($(strip $(FREETZ_PACKAGE_BINARY_TOOLS_STRINGS)),y)
+BINARY_TOOLS_SELECTED += strings
+BINARY_TOOLS_TARGETS_PRECOMPILED += $(BINARY_TOOLS_STRINGS_TARGET)
+$(PKG)_EXCLUDED := $(filter-out usr/bin/strings,$($(PKG)_EXCLUDED))
+else
+$(PKG)_EXCLUDED += usr/bin/strings
+endif
+
+ifeq ($(strip $(FREETZ_PACKAGE_BINARY_TOOLS_AR)),y)
+BINARY_TOOLS_SELECTED += ar
+BINARY_TOOLS_TARGETS_PRECOMPILED += $(BINARY_TOOLS_AR_TARGET)
+$(PKG)_EXCLUDED := $(filter-out usr/bin/ar,$($(PKG)_EXCLUDED))
+else
+$(PKG)_EXCLUDED += usr/bin/ar
+endif
+
+ifeq ($(strip $(FREETZ_PACKAGE_BINARY_TOOLS_RANLIB)),y)
+BINARY_TOOLS_SELECTED += ranlib
+BINARY_TOOLS_TARGETS_PRECOMPILED += $(BINARY_TOOLS_RANLIB_TARGET)
+$(PKG)_EXCLUDED := $(filter-out usr/bin/ranlib,$($(PKG)_EXCLUDED))
+else
+$(PKG)_EXCLUDED += usr/bin/ranlib
+endif
+
+ifeq ($(strip $(FREETZ_PACKAGE_BINARY_TOOLS_STRIP)),y)
+BINARY_TOOLS_SELECTED += strip
+BINARY_TOOLS_TARGETS_PRECOMPILED += $(BINARY_TOOLS_STRIP_TARGET)
+$(PKG)_EXCLUDED := $(filter-out usr/bin/strip,$($(PKG)_EXCLUDED))
+else
+$(PKG)_EXCLUDED += usr/bin/strip
+endif
+
+ifeq ($(strip $(FREETZ_PACKAGE_BINARY_TOOLS_ADDR2LINE)),y)
+BINARY_TOOLS_SELECTED += addr2line
+BINARY_TOOLS_TARGETS_PRECOMPILED += $(BINARY_TOOLS_ADDR2LINE_TARGET)
+$(PKG)_EXCLUDED := $(filter-out usr/bin/addr2line,$($(PKG)_EXCLUDED))
+else
+$(PKG)_EXCLUDED += usr/bin/addr2line
+endif
+
+ifeq ($(strip $(FREETZ_PACKAGE_BINARY_TOOLS_SIZE)),y)
+BINARY_TOOLS_SELECTED += size
+BINARY_TOOLS_TARGETS_PRECOMPILED += $(BINARY_TOOLS_SIZE_TARGET)
+$(PKG)_EXCLUDED := $(filter-out usr/bin/size,$($(PKG)_EXCLUDED))
+else
+$(PKG)_EXCLUDED += usr/bin/size
+endif
+
+ifeq ($(strip $(FREETZ_PACKAGE_BINARY_TOOLS_PATCHELF)),y)
+BINARY_TOOLS_SELECTED += patchelf
+BINARY_TOOLS_TARGETS_PRECOMPILED += $(BINARY_TOOLS_PATCHELF_TARGET)
+$(PKG)_EXCLUDED := $(filter-out usr/bin/patchelf,$($(PKG)_EXCLUDED))
+else
+$(PKG)_EXCLUDED += usr/bin/patchelf
+endif
 
 $(PKG)_CONFIGURE_OPTIONS += --target=$(REAL_GNU_TARGET_NAME)
 $(PKG)_CONFIGURE_OPTIONS += --disable-shared
@@ -62,18 +156,16 @@ $(PKG)_CONFIGURE_OPTIONS += --enable-deterministic-archives
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
 
-# Unpack patchelf from the same tarball used by patchelf-host
-# We depend on patchelf-host download, then unpack to our own build directory
-$($(PKG)_PATCHELF_DIR)/.unpacked: $(DL_DIR)/$(PATCHELF_HOST_SOURCE) | $($(PKG)_SOURCE_DIR) $(UNPACK_TARBALL_PREREQUISITES)
-	@$(call _ECHO,unpacking patchelf for target)
+# Download and unpack patchelf 0.18.0 (no patches needed with dynamic libstdc++)
+$(BINARY_TOOLS_PATCHELF_DIR)/.unpacked: $(DL_DIR)/$(BINARY_TOOLS_PATCHELF_SOURCE) | $(BINARY_TOOLS_SOURCE_DIR) $(UNPACK_TARBALL_PREREQUISITES)
+	@$(call _ECHO,unpacking patchelf $(BINARY_TOOLS_PATCHELF_VERSION) for target)
 	$(RM) -r $(BINARY_TOOLS_PATCHELF_DIR)
 	mkdir -p $(BINARY_TOOLS_PATCHELF_DIR)
-	$(call UNPACK_TARBALL,$(DL_DIR)/$(PATCHELF_HOST_SOURCE),$(BINARY_TOOLS_PATCHELF_DIR),1)
-	$(call APPLY_PATCHES,$(PATCHELF_HOST_MAKE_DIR)/patches/$(PATCHELF_HOST_CONDITIONAL_PATCHES),$(BINARY_TOOLS_PATCHELF_DIR))
+	$(call UNPACK_TARBALL,$(DL_DIR)/$(BINARY_TOOLS_PATCHELF_SOURCE),$(BINARY_TOOLS_PATCHELF_DIR),1)
 	touch $@
 
-# Custom configuration for binutils (not using PKG_CONFIGURED_CONFIGURE to keep manual control)
-$($(PKG)_DIR)/.configured: $($(PKG)_DIR)/.unpacked | $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libc.a
+# Custom configuration for binutils
+$(BINARY_TOOLS_DIR)/.configured: $(BINARY_TOOLS_DIR)/.unpacked | $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libc.a
 	mkdir -p $(BINARY_TOOLS_DIR)
 	(cd $(BINARY_TOOLS_DIR); rm -f config.cache; \
 		$(TARGET_CONFIGURE_ENV) \
@@ -85,65 +177,67 @@ $($(PKG)_DIR)/.configured: $($(PKG)_DIR)/.unpacked | $(TARGET_TOOLCHAIN_STAGING_
 	);
 	touch $@
 
-# Patchelf separate configuration (cross-compile for MIPS target)
-$($(PKG)_PATCHELF_DIR)/.configured: $($(PKG)_PATCHELF_DIR)/.unpacked | $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libc.a
+# Patchelf configuration (bypass wrapper to use libstdc++ instead of uClibc++)
+# Use dynamic linking with libstdc++ from /usr/lib/freetz (RPATH already configured)
+$(BINARY_TOOLS_PATCHELF_DIR)/.configured: $(BINARY_TOOLS_PATCHELF_DIR)/.unpacked | $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libc.a
 	(cd $(BINARY_TOOLS_PATCHELF_DIR); \
 		$(AUTORECONF) \
 		rm -f config.cache; \
 		$(TARGET_CONFIGURE_ENV) \
+		CXX="$(TARGET_CROSS)g++" \
 		./configure \
 		$(TARGET_CONFIGURE_OPTIONS) \
 	);
 	touch $@
 
-$($(PKG)_READELF_BUILD) $($(PKG)_OBJDUMP_BUILD) $($(PKG)_OBJCOPY_BUILD) $($(PKG)_NM_BUILD) $($(PKG)_STRINGS_BUILD) $($(PKG)_AR_BUILD) $($(PKG)_RANLIB_BUILD) $($(PKG)_STRIP_BUILD) $($(PKG)_ADDR2LINE_BUILD) $($(PKG)_SIZE_BUILD): $($(PKG)_DIR)/.configured
+$(BINARY_TOOLS_READELF_BUILD) $(BINARY_TOOLS_OBJDUMP_BUILD) $(BINARY_TOOLS_OBJCOPY_BUILD) $(BINARY_TOOLS_NM_BUILD) $(BINARY_TOOLS_STRINGS_BUILD) $(BINARY_TOOLS_AR_BUILD) $(BINARY_TOOLS_RANLIB_BUILD) $(BINARY_TOOLS_STRIP_BUILD) $(BINARY_TOOLS_ADDR2LINE_BUILD) $(BINARY_TOOLS_SIZE_BUILD): $(BINARY_TOOLS_DIR)/.configured
 	$(SUBMAKE) -C $(BINARY_TOOLS_DIR)
 
-$($(PKG)_PATCHELF_BUILD): $($(PKG)_PATCHELF_DIR)/.configured
+$(BINARY_TOOLS_PATCHELF_BUILD): $(BINARY_TOOLS_PATCHELF_DIR)/.configured
 	$(SUBMAKE) -C $(BINARY_TOOLS_PATCHELF_DIR)
 
-$($(PKG)_READELF_TARGET): $($(PKG)_READELF_BUILD)
+$(BINARY_TOOLS_READELF_TARGET): $(BINARY_TOOLS_READELF_BUILD)
 	$(INSTALL_BINARY_STRIP)
 
-$($(PKG)_OBJDUMP_TARGET): $($(PKG)_OBJDUMP_BUILD)
+$(BINARY_TOOLS_OBJDUMP_TARGET): $(BINARY_TOOLS_OBJDUMP_BUILD)
 	$(INSTALL_BINARY_STRIP)
 
-$($(PKG)_OBJCOPY_TARGET): $($(PKG)_OBJCOPY_BUILD)
+$(BINARY_TOOLS_OBJCOPY_TARGET): $(BINARY_TOOLS_OBJCOPY_BUILD)
 	$(INSTALL_BINARY_STRIP)
 
-$($(PKG)_NM_TARGET): $($(PKG)_NM_BUILD)
+$(BINARY_TOOLS_NM_TARGET): $(BINARY_TOOLS_NM_BUILD)
 	$(INSTALL_BINARY_STRIP)
 
-$($(PKG)_STRINGS_TARGET): $($(PKG)_STRINGS_BUILD)
+$(BINARY_TOOLS_STRINGS_TARGET): $(BINARY_TOOLS_STRINGS_BUILD)
 	$(INSTALL_BINARY_STRIP)
 
-$($(PKG)_AR_TARGET): $($(PKG)_AR_BUILD)
+$(BINARY_TOOLS_AR_TARGET): $(BINARY_TOOLS_AR_BUILD)
 	$(INSTALL_BINARY_STRIP)
 
-$($(PKG)_RANLIB_TARGET): $($(PKG)_RANLIB_BUILD)
+$(BINARY_TOOLS_RANLIB_TARGET): $(BINARY_TOOLS_RANLIB_BUILD)
 	$(INSTALL_BINARY_STRIP)
 
-$($(PKG)_STRIP_TARGET): $($(PKG)_STRIP_BUILD)
+$(BINARY_TOOLS_STRIP_TARGET): $(BINARY_TOOLS_STRIP_BUILD)
 	$(INSTALL_BINARY_STRIP)
 
-$($(PKG)_ADDR2LINE_TARGET): $($(PKG)_ADDR2LINE_BUILD)
+$(BINARY_TOOLS_ADDR2LINE_TARGET): $(BINARY_TOOLS_ADDR2LINE_BUILD)
 	$(INSTALL_BINARY_STRIP)
 
-$($(PKG)_SIZE_TARGET): $($(PKG)_SIZE_BUILD)
+$(BINARY_TOOLS_SIZE_TARGET): $(BINARY_TOOLS_SIZE_BUILD)
 	$(INSTALL_BINARY_STRIP)
 
-$($(PKG)_PATCHELF_TARGET): $($(PKG)_PATCHELF_BUILD)
+$(BINARY_TOOLS_PATCHELF_TARGET): $(BINARY_TOOLS_PATCHELF_BUILD)
 	$(INSTALL_BINARY_STRIP)
 
 $(pkg):
 
-$(pkg)-precompiled: $($(PKG)_READELF_TARGET) $($(PKG)_OBJDUMP_TARGET) $($(PKG)_OBJCOPY_TARGET) $($(PKG)_NM_TARGET) $($(PKG)_STRINGS_TARGET) $($(PKG)_AR_TARGET) $($(PKG)_RANLIB_TARGET) $($(PKG)_STRIP_TARGET) $($(PKG)_ADDR2LINE_TARGET) $($(PKG)_SIZE_TARGET) $($(PKG)_PATCHELF_TARGET)
+$(pkg)-precompiled: $(BINARY_TOOLS_TARGETS_PRECOMPILED)
 
 $(pkg)-clean:
 	-$(SUBMAKE) -C $(BINARY_TOOLS_DIR) clean
 	-$(SUBMAKE) -C $(BINARY_TOOLS_PATCHELF_DIR) clean
 
 $(pkg)-uninstall:
-	$(RM) $($(PKG)_READELF_TARGET) $($(PKG)_OBJDUMP_TARGET) $($(PKG)_OBJCOPY_TARGET) $($(PKG)_NM_TARGET) $($(PKG)_STRINGS_TARGET) $($(PKG)_AR_TARGET) $($(PKG)_RANLIB_TARGET) $($(PKG)_STRIP_TARGET) $($(PKG)_ADDR2LINE_TARGET) $($(PKG)_SIZE_TARGET) $($(PKG)_PATCHELF_TARGET)
+	$(RM) $(BINARY_TOOLS_READELF_TARGET) $(BINARY_TOOLS_OBJDUMP_TARGET) $(BINARY_TOOLS_OBJCOPY_TARGET) $(BINARY_TOOLS_NM_TARGET) $(BINARY_TOOLS_STRINGS_TARGET) $(BINARY_TOOLS_AR_TARGET) $(BINARY_TOOLS_RANLIB_TARGET) $(BINARY_TOOLS_STRIP_TARGET) $(BINARY_TOOLS_ADDR2LINE_TARGET) $(BINARY_TOOLS_SIZE_TARGET) $(BINARY_TOOLS_PATCHELF_TARGET)
 
 $(PKG_FINISH)
