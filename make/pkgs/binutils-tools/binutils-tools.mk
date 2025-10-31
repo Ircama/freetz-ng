c402702b8a40b2d8b2352aa2492ca4f52eade59b
Ircama <Ircama>
2025-10-31 16:18:19 +0100

    binutils-tools: Disable gprofng on uClibc targets

--- make/pkgs/binutils-tools/binutils-tools.mk
+++ make/pkgs/binutils-tools/binutils-tools.mk
@@ -28,6 +28,8 @@ $(PKG)_CONFIGURE_OPTIONS += --disable-sim
 $(PKG)_CONFIGURE_OPTIONS += --disable-gdb
 $(PKG)_CONFIGURE_OPTIONS += --without-included-gettext
 $(PKG)_CONFIGURE_OPTIONS += --enable-deterministic-archives
+# Disable gprofng on uClibc (missing dlvsym, Dl_serinfo, RTLD_DI_SERINFOSIZE)
+$(PKG)_CONFIGURE_OPTIONS += $(if $(FREETZ_AVM_PROP_LIBC_UCLIBC),--disable-gprofng,--enable-gprofng)
 
 
 $(PKG_SOURCE_DOWNLOAD)
