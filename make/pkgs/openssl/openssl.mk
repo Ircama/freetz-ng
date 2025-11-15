877f24b9c0332120e63b09d7d0675210b4e3aaff
Ircama <Ircama>
2025-11-15 16:03:39 +0100

    fix openssl

--- make/pkgs/openssl/openssl.mk
+++ make/pkgs/openssl/openssl.mk
@@ -81,10 +81,6 @@ $(PKG)_MAKE_FLAGS += SHARED_LDFLAGS=""
 $(PKG)_MAKE_FLAGS += $(if $(FREETZ_OPENSSL_VERSION_10_MAX),INSTALL_PREFIX,DESTDIR)="$(TARGET_TOOLCHAIN_STAGING_DIR)"
 $(PKG)_MAKE_FLAGS += CROSS_COMPILE=1
 $(PKG)_MAKE_FLAGS += $(if $(FREETZ_PACKAGE_OPENSSL_STATIC),STATIC_APPS=1)
-# OpenSSL 0.9.8 has race conditions in parallel builds - force sequential
-ifeq ($(strip $(FREETZ_OPENSSL_VERSION_09_MAX)),y)
-$(PKG)_MAKE_FLAGS += -j1
-endif
 
 
 # openssl-host and openssl using the same source
@@ -101,12 +97,7 @@ $($(PKG)_BINARY_BUILD_DIR) $($(PKG)_LIBS_BUILD_DIR): $($(PKG)_DIR)/.configured
 #	that it doesn't contain files from previous builds (0.9.8 to/from 1.0.x switch).
 	$(MAKE) openssl-clean-staging openssl-uninstall $(SILENT)
 	$(SUBMAKE) $(OPENSSL_MAKE_FLAGS) depend
-ifeq ($(strip $(FREETZ_OPENSSL_VERSION_09_MAX)),y)
-	# OpenSSL 0.9.8: Force serial build to avoid race condition
-	$(SUBMAKE) -j1 $(OPENSSL_MAKE_FLAGS) all
-else
 	$(SUBMAKE) $(OPENSSL_MAKE_FLAGS) all
-endif
 
 $($(PKG)_LIBS_STAGING_DIR): $($(PKG)_LIBS_BUILD_DIR)
 	$(SUBMAKE) $(OPENSSL_MAKE_FLAGS) $(if $(FREETZ_OPENSSL_VERSION_10_MAX),install,install_sw)
