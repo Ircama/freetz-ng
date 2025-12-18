fa14fa83e18b4ef00683f77b7fd55f2512f01f1f
Ircama <Ircama>
2025-12-18 22:33:17 +0100

    tree - fix download failure on old devices

--- make/pkgs/tree/tree.mk
+++ make/pkgs/tree/tree.mk
@@ -1,10 +1,12 @@
 $(call PKG_INIT_BIN, $(if $(FREETZ_PACKAGE_TREE_VERSION_ABANDON),1.8.0,2.2.1))
-$(PKG)_SOURCE_DOWNLOAD_NAME:=$(if $(FREETZ_PACKAGE_TREE_VERSION_ABANDON),,unix-$($(PKG)_VERSION).tar.gz)
+$(PKG)_SOURCE_DOWNLOAD_NAME:=$(if $(FREETZ_PACKAGE_TREE_VERSION_ABANDON),tree_$($(PKG)_VERSION).orig.tar.gz,unix-$($(PKG)_VERSION).tar.gz)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tgz
 $(PKG)_HASH_ABANDON:=715d5d4b434321ce74706d0dd067505bb60c5ea83b5f0b3655dae40aa6f9b7c2
 $(PKG)_HASH_CURRENT:=eb152b9b9b49c3ed9a08094f386ddc0aaddecce0ae45fbd58cb7680d0db4068a
 $(PKG)_HASH:=$($(PKG)_HASH_$(if $(FREETZ_PACKAGE_TREE_VERSION_ABANDON),ABANDON,CURRENT))
-$(PKG)_SITE:=https://gitlab.com/OldManProgrammer/unix-tree/-/archive/$($(PKG)_VERSION),https://mama.indstate.edu/users/ice/tree/src,ftp://mama.indstate.edu/linux/tree
+$(PKG)_SITE_ABANDON:=https://ftp.debian.org/debian/pool/main/t/tree/
+$(PKG)_SITE_CURRENT:=https://gitlab.com/OldManProgrammer/unix-tree/-/archive/$($(PKG)_VERSION)
+$(PKG)_SITE:=$($(PKG)_SITE_$(if $(FREETZ_PACKAGE_TREE_VERSION_ABANDON),ABANDON,CURRENT))
 ### WEBSITE:=http://mama.indstate.edu/users/ice/tree/
 ### MANPAGE:=https://linux.die.net/man/1/tree
 ### CHANGES:=https://gitlab.com/OldManProgrammer/unix-tree/tags
